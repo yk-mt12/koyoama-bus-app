0a265e1f74f8b6121c80dc6e59aa6242a5f1e511
yk-mt12 <agvksl543@gmail.com>
2023-06-01 17:30:38 +0900

    feat: データフェッチの処理を分離した

---
+++ src/hooks/useContent.ts
@@ -0,0 +1,19 @@
+import useSWR from 'swr';
+
+// fetcher関数の作成
+const fetcher = (url: RequestInfo | URL) => fetch(url).then((r) => r.json());
+
+// ユーザー情報の取得
+function useContent() {
+  const { data, error } = useSWR(
+    `https://sheets.googleapis.com/v4/spreadsheets/${import.meta.env.VITE_SPREADSHEET_ID}/values/all?key=${import.meta.env.VITE_API_KEY}`,
+    fetcher
+  );
+  return {
+    data: data,
+    isLoading: !error && !data,
+    isError: error,
+  };
+}
+
+export default useContent;
