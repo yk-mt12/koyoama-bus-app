8da39f2294a4a4a302026b8538cc478787894590
yk-mt12 <agvksl543@gmail.com>
2023-06-02 23:58:27 +0900

    refactor: フィルタリングをapp.tsxに移行し、フィルタリングされたデータを渡すように 変更

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,44 @@ import Header from "./components/Header/";
 import formatContents from "./hooks/formatContents.ts";
 import useContent from "./hooks/useContent.ts";
 import Loading from "./components/Loading";
+import { useRecoilValue } from "recoil";
+import {
+  activeTabState,
+  scheduleTypeState,
+  selectedDirectionState,
+  selectedRouteState,
+} from "./state/atoms.ts";
 
 const App = () => {
+  const activeTab = useRecoilValue(activeTabState);
+  const selectedDirection = useRecoilValue(selectedDirectionState);
+  const day = useRecoilValue(scheduleTypeState);
+  const selectedRoute = useRecoilValue(selectedRouteState);
+
   const { data, isLoading } = useContent();
   const formatedData = data ? formatContents(data?.values) : [];
+
+  // Filter data based on the provided day, activeTab, and selectedDirection
+  const scheduleData = formatedData.filter((item: any) => {
+    console.log(activeTab, selectedDirection, selectedRoute);
+    return (
+      item.day === day &&
+      item.route === selectedRoute &&
+      ((activeTab === "上賀茂神社" &&
+        selectedDirection === "going" &&
+        item.route === "上賀茂神社→大学") ||
+        (activeTab === "上賀茂神社" &&
+          selectedDirection === "returning" &&
+          item.route === "大学→上賀茂神社") ||
+        (activeTab === "二軒茶屋" &&
+          selectedDirection === "going" &&
+          item.route === "二軒茶屋→大学") ||
+        (activeTab === "二軒茶屋" &&
+          selectedDirection === "returning" &&
+          item.route === "大学→二軒茶屋"))
+    );
+  });
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
@@ -29,7 +63,7 @@ const App = () => {
             <Loading />
           </div>
         ) : (
-          <TimeTable data={formatedData} />
+          <TimeTable data={scheduleData} />
         )}
       </div>
     </div>
