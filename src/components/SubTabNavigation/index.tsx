6440090c762d22e6732bd1a83423be0c8e3bfedf
yk-mt12 <agvksl543@gmail.com>
2023-06-03 18:50:31 +0900

    fix: 時刻表が行きと帰りで逆になるバグを修正

--- src/components/SubTabNavigation/index.tsx
+++ src/components/SubTabNavigation/index.tsx
@@ -34,21 +34,21 @@ const SubTabNavigation: React.FC = () => {
     <div className="flex justify-center space-x-4">
       <button
         className={`py-2 px-4 rounded-md ${
-          selectedDirection === "going"
-            ? "bg-blue-500 text-white"
-            : "bg-gray-200 text-gray-800"
-        }`}
-        onClick={() => handleDirectionChange("going")}
-      >
-        行き
-      </button>
-      <button
-        className={`py-2 px-4 rounded-md ${
           selectedDirection === "returning"
             ? "bg-blue-500 text-white"
             : "bg-gray-200 text-gray-800"
         }`}
         onClick={() => handleDirectionChange("returning")}
+      >
+        行き
+      </button>
+      <button
+        className={`py-2 px-4 rounded-md ${
+          selectedDirection === "going"
+            ? "bg-blue-500 text-white"
+            : "bg-gray-200 text-gray-800"
+        }`}
+        onClick={() => handleDirectionChange("going")}
       >
         帰り
       </button>
