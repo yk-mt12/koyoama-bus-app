b44dcf058241c8300fbc4dac6d68aa9261255137
yk-mt12 <agvksl543@gmail.com>
2023-06-07 07:35:09 +0900

    style: headerのテキストサイズを変更

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,8 +2,8 @@ const Header = () => {
   return (
     <header className="bg-white shadow">
       <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <h1 className="text-3xl font-bold text-gray-900">
-          神山シャトルン 時刻表
+        <h1 className="text-xl md:text-3xl font-bold text-gray-900">
+          上賀茂・二軒茶屋シャトルバス時刻表
         </h1>
       </div>
     </header>
