f207413a7c6fcdcf7c90c455725504141311978b
yk-mt12 <agvksl543@gmail.com>
2023-06-07 07:55:38 +0900

    chore: 本番環境時にdeveloper toolsを参照できないように設定

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { RecoilRoot } from "recoil";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (import.meta.env.MODE === "production") {
+  disableReactDevTools();
+}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
