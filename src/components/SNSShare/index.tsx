b7882a4a67c749598bc62021b2ed34614f88682a
kitsune <agvksl543@gmail.com>
2023-06-26 13:18:43 +0900

    feat: create share button(line, facebook, twitter)

---
+++ src/components/SNSShare/index.tsx
@@ -0,0 +1,37 @@
+import {
+  FacebookIcon,
+  FacebookShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+  LineIcon,
+  LineShareButton,
+} from "react-share";
+
+const SNSShare = () => {
+  const url = "https://kyoto-su-bus.vercel.app/";
+  const title =
+    "京都産業大学 神山シャトルン 上賀茂シャトルバス・二軒茶屋シャトルバス時刻表";
+  const iconSize = 40;
+  return (
+    // コンポーネントを中央揃えにする
+    <div className="">
+      <FacebookShareButton url={url} title={title}>
+        <FacebookIcon size={iconSize} round />
+      </FacebookShareButton>
+      <TwitterShareButton
+        url={url}
+        title={title}
+        via="kyosan_bus"
+        related={["kyosan_bus"]}
+        hashtags={["京都産業大学", "京産", "神山シャトルン"]}
+      >
+        <TwitterIcon size={iconSize} round />
+      </TwitterShareButton>
+      <LineShareButton title={title} url={url}>
+        <LineIcon size={iconSize} round />
+      </LineShareButton>
+    </div>
+  );
+};
+
+export default SNSShare;
