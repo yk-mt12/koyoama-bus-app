d451ba120c283ab0fcb4e75a6e9860504c0c6ab7
kitsune <agvksl543@gmail.com>
2023-06-27 15:25:44 +0900

    feat: add button for friend in line

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
+import { LineIcon } from "react-share";
+
 const Header = () => {
   return (
-    <header className="bg-white shadow">
+    <header className="bg-white shadow flex items-center pr-4">
       <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
         <h1 className="text-xl md:text-3xl font-bold text-gray-900">
           京都産業大学 神山シャトルン
@@ -9,6 +11,9 @@ const Header = () => {
           上賀茂シャトルバス・二軒茶屋シャトルバス時刻表
         </h2>
       </div>
+      <a href="https://lin.ee/oWjPFcc">
+        <LineIcon borderRadius={12} size={42} />
+      </a>
     </header>
   );
 };
